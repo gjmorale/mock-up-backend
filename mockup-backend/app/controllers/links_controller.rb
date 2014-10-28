e7eab377fc7e9cc88834acc57f3ebcdb995f6398
Guillermo Morales <gjmorale@uc.cl>
2014-10-28 08:59:49 -0300

    con seed y arreglo de bugs

--- mockup-backend/app/controllers/links_controller.rb
+++ mockup-backend/app/controllers/links_controller.rb
@@ -69,6 +69,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:value, :link, :title, :mashup_id, :source_link_id)
+      params.require(:link).permit(:value, :link, :title, :mashup_id, :link_source_id)
     end
 end
--- mockup-backend/app/views/links/index.json.jbuilder
+++ mockup-backend/app/views/links/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@links) do |link|
-  json.extract! link, :id, :value, :link, :title, :mashup_id, :source_link_id
+  json.extract! link, :id, :value, :link, :title, :mashup_id, :link_source_id
   json.url link_url(link, format: :json)
 end
